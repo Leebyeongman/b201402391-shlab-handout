c3941cdbbb7960c5e6f258b203bf4e1ede57673c
Leebyeongman <ssey0921@cnu.ac.kr>
2017-11-16 04:16:48 +0900

    trace02

--- tsh.c
+++ tsh.c
@@ -2,7 +2,7 @@
  * tsh - A tiny shell program with job control
  *
  *
- * 201402391 이병만
+ * 201402391 이병만 
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -169,11 +169,30 @@ int main(int argc, char **argv)
  */
 void eval(char *cmdline) 
 {
+	char *argv[MAXARGS];
+	pid_t pid;
+
+	parseline(cmdline, argv);
+	pid = fork();
+	if(!builtin_cmd(argv)){
+		if(pid == 0){
+			if((execve(argv[0], argv, environ) < 0)){
+				printf("%s, Command not found\n", argv);
+				exit(0);
+			}
+		}
+	}
 	return;
 }
 
 int builtin_cmd(char **argv)
 {
+	char *cmd = argv[0];
+
+	if(!strcmp(cmd, "quit")){
+		exit(0);
+	}
+
 	return 0;
 }
 
